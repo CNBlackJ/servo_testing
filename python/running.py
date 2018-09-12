23ff10afe0cfe43cd84856007ae39f50ce71d17e
CNBlackJ <yes.heng@icloud.com>
2018-09-12 12:33:38 +0800

    chore: update logic

--- python/running.py
+++ python/running.py
@@ -9,13 +9,15 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+p.start(0) # Initialization
 try:
   while True:
-    p.ChangeDutyCycle(0)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(30)
-    time.sleep(0.5)
+  	for dc in range(0, 101, 5):
+  		p.ChangeDutyCycle(dc)
+  		time.sleep(0.1)
+  	for dc in range(100, -1, -5):
+  		p.ChangeDutyCycle(dc)
+  		time.sleep(0.1)
 except KeyboardInterrupt:
   p.stop()
   GPIO.cleanup()
