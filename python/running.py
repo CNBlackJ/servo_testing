01b8e2ecd6b658e691ae63d0d1307701322ff4dd
CNBlackJ <yes.heng@icloud.com>
2018-09-12 16:00:50 +0800

    feat: update dc

--- python/running.py
+++ python/running.py
@@ -12,12 +12,12 @@ p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(0) # Initialization
 try:
   while True:
-  	for dc in range(0, 100, 5):
-  		p.ChangeDutyCycle(dc)
-  		time.sleep(0.05)
-  	for dc in range(100, -1, -5):
-  		p.ChangeDutyCycle(dc)
-  		time.sleep(0.05)
+    p.ChangeDutyCycle(7.5)  # turn towards 90 degree
+    time.sleep(1) # sleep 1 second
+    p.ChangeDutyCycle(2.5)  # turn towards 0 degree
+    time.sleep(1) # sleep 1 second
+    p.ChangeDutyCycle(12.5) # turn towards 180 degree
+    time.sleep(1) # sleep 1 second 
 except KeyboardInterrupt:
   p.stop()
   GPIO.cleanup()
