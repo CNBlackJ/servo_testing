46552baaf9a12a928a835b9e584c21bf65fcbc2d
CNBlackJ <yes.heng@icloud.com>
2018-09-12 11:12:57 +0800

    chore: change servo ping

--- python/running.py
+++ python/running.py
@@ -1,29 +1,33 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python    
 
-import RPi.GPIO as GPIO  
-import time  
-import signal  
-import atexit  
+import RPi.GPIO as GPIO
+import time
 
-atexit.register(GPIO.cleanup)    
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
 
-servopin = 21  
-GPIO.setmode(GPIO.BCM)  
-GPIO.setup(servopin, GPIO.OUT, initial=False)  
-p = GPIO.PWM(servopin,50) #50HZ  
-p.start(0)  
-time.sleep(2)  
-
-while(True):  
-  for i in range(0,181,10):  
-    p.ChangeDutyCycle(2.5 + 10 * i / 180) #设置转动角度  
-    time.sleep(0.02)                      #等该20ms周期结束  
-    p.ChangeDutyCycle(0)                  #归零信号  
-    time.sleep(0.2)  
-
-  for i in range(181,0,-10):  
-    p.ChangeDutyCycle(2.5 + 10 * i / 180)  
-    time.sleep(0.02)  
-    p.ChangeDutyCycle(0)  
-    time.sleep(0.2)
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(2.5) # Initialization
+try:
+  while True:
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(12.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(2.5)
+    time.sleep(0.5)
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
