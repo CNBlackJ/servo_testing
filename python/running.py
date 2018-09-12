ce0f6a318af5dbe5f4267d02c053989b16444833
CNBlackJ <yes.heng@icloud.com>
2018-09-12 16:02:24 +0800

    feat: 0 - 180 degree

--- python/running.py
+++ python/running.py
@@ -12,12 +12,12 @@ p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(0) # Initialization
 try:
   while True:
-    p.ChangeDutyCycle(7.5)  # turn towards 90 degree
-    time.sleep(1) # sleep 1 second
+    # p.ChangeDutyCycle(7.5)  # turn towards 90 degree
+    # time.sleep(1) # sleep 1 second
     p.ChangeDutyCycle(2.5)  # turn towards 0 degree
-    time.sleep(1) # sleep 1 second
+    time.sleep(0.5) # sleep 1 second
     p.ChangeDutyCycle(12.5) # turn towards 180 degree
-    time.sleep(1) # sleep 1 second 
+    time.sleep(0.5) # sleep 1 second 
 except KeyboardInterrupt:
   p.stop()
   GPIO.cleanup()
