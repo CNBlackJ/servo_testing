2641d3e9282a369325388fdbf83ab8a29a0b0989
CNBlackJ <yes.heng@icloud.com>
2018-09-12 12:21:10 +0800

    chore: change dc value

--- python/running.py
+++ python/running.py
@@ -12,9 +12,9 @@ p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(2.5) # Initialization
 try:
   while True:
-    p.ChangeDutyCycle(5)
+    p.ChangeDutyCycle(0)
     time.sleep(0.5)
-    p.ChangeDutyCycle(8)
+    p.ChangeDutyCycle(100)
     time.sleep(0.5)
 except KeyboardInterrupt:
   p.stop()
