7dde016d2c1e2edac09f60d6827bfacb59714466
CNBlackJ <yes.heng@icloud.com>
2018-09-12 16:04:09 +0800

    chore: updte sleep time

--- python/running.py
+++ python/running.py
@@ -15,9 +15,9 @@ try:
     # p.ChangeDutyCycle(7.5)  # turn towards 90 degree
     # time.sleep(1) # sleep 1 second
     p.ChangeDutyCycle(2.5)  # turn towards 0 degree
-    time.sleep(0.5) # sleep 1 second
+    time.sleep(0.2) # sleep 1 second
     p.ChangeDutyCycle(12.5) # turn towards 180 degree
-    time.sleep(0.5) # sleep 1 second 
+    time.sleep(0.2) # sleep 1 second 
 except KeyboardInterrupt:
   p.stop()
   GPIO.cleanup()
