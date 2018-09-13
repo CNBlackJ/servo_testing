0a4bc5b6c5ef9cb5457c80602adaa0fbaab04ff6
CNBlackJ <yes.heng@icloud.com>
2018-09-13 10:05:00 +0800

    feat: add three servo contaol

--- python/running.py
+++ python/running.py
@@ -4,20 +4,33 @@
 import RPi.GPIO as GPIO
 import time
 
-servoPIN = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
+servoPIN1 = 17
+servoPIN2 = 27
+servoPIN3 = 22
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(0) # Initialization
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN1, GPIO.OUT)
+GPIO.setup(servoPIN2, GPIO.OUT)
+GPIO.setup(servoPIN3, GPIO.OUT)
+
+p1 = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p2 = GPIO.PWM(servoPIN, 50)
+p3 = GPIO.PWM(servoPIN, 50)
+p1.start(0) # Initialization
+p2.start(0)
+p3.start(0)
 try:
   while True:
     # p.ChangeDutyCycle(7.5)  # turn towards 90 degree
     # time.sleep(1) # sleep 1 second
-    p.ChangeDutyCycle(2.5)  # turn towards 0 degree
-    time.sleep(0.2) # sleep 1 second
-    p.ChangeDutyCycle(12.5) # turn towards 180 degree
-    time.sleep(0.2) # sleep 1 second 
+    p1.ChangeDutyCycle(2.5)  # turn towards 0 degree
+    p2.ChangeDutyCycle(2.5)
+    p3.ChangeDutyCycle(2.5)
+    time.sleep(0.5) # sleep 1 second
+    p1.ChangeDutyCycle(12.5) # turn towards 180 degree
+    p2.ChangeDutyCycle(12.5)
+    p3.ChangeDutyCycle(12.5)
+    time.sleep(0.5) # sleep 1 second 
 except KeyboardInterrupt:
   p.stop()
   GPIO.cleanup()
